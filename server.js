72b6e33bfbaece3adbfe6abf0c431a3ef3bc62da
mrt2000HW <71808733+mrt2000HW@users.noreply.github.com>
2020-11-26 10:04:21 +0400

    check 64

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.get("/",function(req,res){
     res.sendFile('index.html', {root: __dirname});
 });
 
-app.get("/game",function(req,res){
-    res.sendFile('game-page.html', {root: __dirname});
-});
+// app.get("/game",function(req,res){
+//     res.sendFile('game-page.html', {root: __dirname});
+// });
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
